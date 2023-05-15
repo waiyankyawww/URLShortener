401bef8dc7de73522d95ff8b5f0d7126d3d52d45
Wai Yan Kyaw <waiyankyaw132000@gmail.com>
2023-05-15 17:56:53 +0630

    index update

--- routes/index.js
+++ routes/index.js
@@ -5,25 +5,24 @@ import hashMap from "./urls.js";
 const router = express.Router();
 
 router.get("/:urlId", async (req, res) => {
-  try {
-    const urlId = req.params.urlId;
-    hashMap.get(urlId);
+  const urlId = req.params.urlId;
 
-    if (url) {
-      await Url.updateOne(
-        {
-          urlId: urlId,
-        },
-        {
-          $inc: { clicks: 1 },
-        }
-      );
-      return res.redirect(url.origUrl);
-    } else res.status(400).json("not found");
-  } catch (error) {
-    console.log(error);
-    res.status(500).json("server error");
-  }
+  const key = getKeyByValue(hashMap, urlId);
+
+  return res.redirect(key);
 });
+
+// define a function to get keys by value
+const getKeyByValue = (map, searchValue) => {
+  const keys = [];
+  const base = process.env.BASE;
+  const shortUrl = `${base}/${searchValue}`;
+  for (const [key, value] of map.entries()) {
+    if (value === shortUrl) {
+      keys.push(key);
+    }
+  }
+  return keys;
+};
 
 export default router;
